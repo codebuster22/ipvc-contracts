7a8f45d1081d7530e49daababb539f6c5635fe7b
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-07-22 11:25:49 +0530

    added functionality to getWarrior and updateController

--- contracts/demo/Warriors.sol
+++ contracts/demo/Warriors.sol
@@ -8,14 +8,14 @@ import "../utils/ERC721.sol";
 contract Warriors is ERC721 {
 
     string public constant token_version = "0.1.0-beta";
-    address public immutable controller;
+    address public controller;
 
-    // tokenId => warriorGene 
+    // tokenId => warriorGene
     // warriorGene - {3 digits for country + 10 digits for characteristics}
     // Gene - (3 digits)+(two digits charateristics)x(5 layers) - total 10 digits
     // Total warriors - 200*99*99*99*99*99 = 19,01,98,00,99,800
-    mapping( uint256 => uint256 ) public warriors;
-    uint256 warriorId;
+    mapping( uint256 => uint256 ) private warriors;
+    uint256 public warriorCounter;
 
     event WarriorGenerated(address indexed owner, uint256 indexed warriorId);
 
@@ -29,9 +29,9 @@ contract Warriors is ERC721 {
 
     /**
      * @dev constructor
-     * @param _controller of controller who is allowed to call write functions
+     * @param _controller controller address who is allowed to call write functions
      */
-    constructor (address _controller) ERC721("VaccineToken","VT") {
+    constructor (address _controller) ERC721("Warriors","WRT") {
         require(
             _controller != address(0),
             "Warriors: Controller address cannot be zero address"
@@ -44,7 +44,7 @@ contract Warriors is ERC721 {
       * @param _gene warrior gene for the token
       * @param _owner address of owner who requested to generate warrior
       */
-    function generateWarrior(uint256 _gene, address _owner) public onlyController {
+    function generateWarrior(uint256 _gene, address _owner) external onlyController {
         require(
             _gene != 0,
             "Warriors: no warrior without gene"
@@ -53,10 +53,34 @@ contract Warriors is ERC721 {
             _owner != address(0),
             "Warriors: no warrior can be assigned to zero address"
         );
-        uint256 tokenId = warriorId;
-        warriorId++;
+        uint256 tokenId = warriorCounter;
+        warriorCounter++;
         warriors[tokenId] = _gene;
         _mint(_owner, tokenId);
         emit WarriorGenerated(_owner, tokenId);
+    }
+
+    /**
+     * @dev update controller address
+     * @param _newController controller address who is allowed to call write functions
+     */
+    function updateController(address _newController) public onlyController {
+        require(
+            _newController!=address(0),
+            "Warriors: controller cannot be address zero"
+        );
+        controller = _newController;
+    }
+
+    /**
+     * @dev get warrior's gene
+     * @param _warriorId id of warrior token
+     */
+    function getWarrior(uint256 _warriorId) external view returns(uint256 warrior) {
+        warrior = warriors[_warriorId];
+        require(
+            warrior != 0,
+            "Warriors: warrior does not exist"
+        );
     }
 }
