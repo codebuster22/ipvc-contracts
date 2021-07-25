0a00aa491e2eb7fd461ae4644b9bb097b11eddb7
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-07-25 23:54:31 +0530

    added docs for functions at WarriorGeneGenerator contract

--- contracts/MVP/WarriorGeneGenerator.sol
+++ contracts/MVP/WarriorGeneGenerator.sol
@@ -23,17 +23,28 @@ contract WarriorGeneGenerator{
         _;
     }
 
+    /**
+     * @dev constructor to set inital value.
+     */
     constructor () {
         controller = msg.sender;
         totalGeneAttributes = 5;
         geneModulus = 10**(5*2);
     }
 
+    /**
+     * @dev generate warrior gene
+     * @param _metadata unique metadata using which the warrior attributes are generated
+     */
     function geneGenerator(bytes32 _metadata) public view returns(uint256 gene){
         gene = uint256(_metadata);
         gene = gene % geneModulus;
     }
 
+    /**
+     * @dev set new total warrior gene attributes
+     * @param _newTotalGeneAttributes total number of attributes a warrior should have
+     */
     function setTotalGeneAttributes(uint8 _newTotalGeneAttributes) public onlyController{
         require(
             _newTotalGeneAttributes > totalGeneAttributes,
@@ -43,6 +54,10 @@ contract WarriorGeneGenerator{
         geneModulus = 10 ** (totalGeneAttributes*2);
     }
 
+    /**
+     * @dev set new controller
+     * @param _newController new controller address
+     */
     function setController(address _newController) public onlyController{
         require(
             _newController != address(0),
