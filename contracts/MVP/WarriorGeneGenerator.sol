62baf8e3555e0873e7c2b13c0794cd71a04e86ff
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-07-25 23:42:46 +0530

    Created GeneGenerator contract to have separate logic for genetic algorithm

--- contracts/MVP/WarriorGeneGenerator.sol
+++ contracts/MVP/WarriorGeneGenerator.sol
@@ -2,5 +2,53 @@ pragma solidity ^0.8.6;
 
 
 contract WarriorGeneGenerator{
-    
+
+    address public controller;
+    uint8 public totalGeneAttributes;
+    uint256 public geneModulus;
+
+    modifier onlyController() {
+        require(
+            msg.sender == controller,
+            "WarriorGeneGenerator: only controller functionality"
+        );
+        _;
+    }
+
+    modifier isInitialized() {
+        require(
+            totalGeneAttributes != 0,
+            "WarriorGeneGenerator: contract uninitialized"
+        );
+        _;
+    }
+
+    constructor () {
+        controller = msg.sender;
+        totalGeneAttributes = 5;
+        geneModulus = 10**(5*2);
+    }
+
+    function geneGenerator(bytes32 _metadata) public view returns(uint256 gene){
+        gene = uint256(_metadata);
+        gene = gene % geneModulus;
+    }
+
+    function setTotalGeneAttributes(uint8 _newTotalGeneAttributes) public onlyController{
+        require(
+            _newTotalGeneAttributes > totalGeneAttributes,
+            "WarriorGeneGenerator: cannot decrease gene attributes"
+        );
+        totalGeneAttributes = _newTotalGeneAttributes;
+        geneModulus = 10 ** (totalGeneAttributes*2);
+    }
+
+    function setController(address _newController) public onlyController{
+        require(
+            _newController != address(0),
+            "WarriorGeneGenerator: new controller cannot be zero"
+        );
+        controller = _newController;
+    }
+
 }
