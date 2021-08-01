6a3d3599546fea35416a85c012b17d67dcbf78bc
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2021-08-01 15:40:57 +0530

    added contracts for access control

---
+++ contracts/access/OriginControl.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.6;
+
+import "./Authorized.sol";
+import "../utils/SignatureHelper.sol";
+
+contract OriginControl is Authorized, SignatureHelper{
+
+    address public origin;                      // address from which the call to generateWarrior can be accepted
+
+    function setOrigin(address _newOrigin) external onlyAdmin{
+        require(
+            _newOrigin != address(0),
+            "Controller: origin cannot be zero address"
+        );
+        origin = _newOrigin;
+    }
+
+    modifier onlyOrigin(
+        address _from,
+        bytes32 _metadata,
+        bytes memory _signature
+    ) {
+        bytes32 messageHash = generateHash(address(this), _from, _metadata);
+        bytes32 ethSignedMessageHash = getEthSignedMessageHash(messageHash);
+        require(
+            recoverSigner(ethSignedMessageHash, _signature) == origin,
+            "Controller: invalid origin"
+        );
+        _;
+    }
+
+    function generateHash(
+        address _to,
+        address _from,
+        bytes32 _metadata
+    ) public pure returns(bytes32) {
+        return keccak256(
+            abi.encodePacked(
+                _to,_from, _metadata, "generateWarrior(address, bytes32, bytes memory)"
+            )
+        );
+    }
+}
